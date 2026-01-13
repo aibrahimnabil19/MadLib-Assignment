b5f4534e7869b53787f14f0e90dba8396ca0fe8a
aibrahimnabil19 <aibrahimnabil@gmail.com>
2026-01-13 21:16:07 +0100

    added a new line to the breakup story

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const occupation = prompt("Enter an occupation: ");
 const mansName = prompt("Which man do you hate the most?: ");
 const sport = prompt("Enter a sport: ");
 
-console.log(`${yourName} broke up with his ex (${exName}) after discovering that every night, ${exName} would ${verb} while asleep and narrate her dreams like a ${occupation}. One night she shouted "${mansName}!" and pretended to score a ${sport} goal. ${yourName} realized love should not come with another man's name and sport commentary.`);
+console.log(`${yourName} broke up with his ex (${exName}) after discovering that every night, ${exName} would ${verb} while asleep and narrate her dreams like a ${occupation}. One night she shouted "${mansName}!" and pretended to score a ${sport} goal. ${yourName} realized love should not come with another man's name and sport commentary so he broke up with her.`);
 
-console.log(yourName + " broke up with his ex (" + exName + ") after discovering that every night," +  exName + " would " + verb + " while asleep and narrate her dreams like a " + occupation + ". One night she shouted \"" + mansName + "!\" and pretended to score a " + sport + " goal. " + yourName + " realized love should not come with another man's name and sport commentary.");
+console.log(yourName + " broke up with his ex (" + exName + ") after discovering that every night," +  exName + " would " + verb + " while asleep and narrate her dreams like a " + occupation + ". One night she shouted \"" + mansName + "!\" and pretended to score a " + sport + " goal. " + yourName + " realized love should not come with another man's name and sport commentary so he broke up with her.");
